a40358dbb777faabaa12df04986bf166e74a9ad5
Javed Ali <Javedali90045@gmail.com>
2024-10-14 11:48:08 +0530

    Added validation

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -6,6 +6,42 @@ import "./LoginSignup.css";
 
 const LoginSignup = () => {
   const [action, setAction] = useState("Login");
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({});
+
+  function Validation(values) {
+    const errors = {};
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+
+    if (values.name === "") {
+      errors.name = "Name is required";
+    }
+    if (values.email === "") {
+      errors.email = 'Email is required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
+    if (values.password === "") {
+      errors.password = "Password is required";
+    } 
+
+    return errors;
+  }
+  function handleInput(e) {
+    const newObj = { ...values, [e.target.name]: e.target.value };
+    console.log(values.name);
+    setValues(newObj);
+  }
+
+  function handleValidation(e) {
+    e.preventDefault();
+    setError(Validation(values));
+  }
 
   return (
     <div className="container">
@@ -15,18 +51,78 @@ const LoginSignup = () => {
       </div>
       <div className="inputs">
         {action === "Login" ? null : (
+          <div className="input-elements">
           <div className="input">
             <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={values.name}
+              onChange={handleInput}
+            />
+            
+          </div>
+          <div className="error">
+          {error.name && (
+              <p
+                style={{
+                  color: "red",
+                }}
+              >
+                {error.name}
+              </p>
+            )}
+          </div>
           </div>
         )}
-        <div className="input">
-          <img src={email_icon} alt="" />
-          <input type="email" placeholder="Email ID" />
+        <div className="input-elements">
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input
+              type="email"
+              placeholder="Email ID"
+              name="email"
+              value={values.email}
+              onChange={handleInput}
+            />
+          </div>
+          <div className="error">
+          {error.email && (
+            <p
+              style={{
+                color: "red",
+              }}
+            >
+              {error.email}
+            </p>
+          )}
+          </div>
+          
         </div>
+
+        <div className="input-elements">
         <div className="input">
           <img src={password_icon} alt="" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={values.password}
+            onChange={handleInput}
+          />
+        </div>
+        <div className="error">
+        {error.password && (
+            <p
+              style={{
+                color: "red",
+              }}
+            >
+              {error.password}
+            </p>
+          )}
+        </div>
         </div>
       </div>
       {action === "Sign Up" ? null : (
@@ -37,13 +133,19 @@ const LoginSignup = () => {
       <div className="submit-container">
         <div
           className={action === "Login" ? "submit gray" : "submit"}
-          onClick={() => setAction("Sign Up")}
+          onClick={(e) => {
+            setAction("Sign Up");
+            handleValidation(e);
+          }}
         >
           Sign Up
         </div>
         <div
           className={action === "Sign Up" ? "submit gray" : "submit"}
-          onClick={() => setAction("Login")}
+          onClick={(e) => {
+            setAction("Login");
+            handleValidation(e);
+          }}
         >
           Login
         </div>
